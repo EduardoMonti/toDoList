e3f3fcec61259cb0872ee5482bd00e3a7fc53890
EduardoMonti <dudumonti@hotmail.com>
2024-08-26 13:19:22 -0300

    Project created

---
+++ src/main/java/br/com/practing/twtodos/TwtodosApplication.java
@@ -0,0 +1,14 @@
+package br.com.practing.twtodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwtodosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TwtodosApplication.class, args);
+	}
+
+}
